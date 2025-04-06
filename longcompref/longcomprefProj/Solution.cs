7366d5917d2e3676b63d955749b0f1e084ec1e7b
nefir2 <vadim.ahtamov@gmail.com>
2025-04-06 21:16:23 +0300

    solved

---
+++ longcompref/longcomprefProj/Solution.cs
@@ -0,0 +1,55 @@
+﻿namespace longcomprefProj
+{
+	public class Solution
+	{
+		internal const int ZERO = 0;
+		public string LongestCommonPrefix(string[] strs)
+		{
+			string prefix = string.Empty;
+			int minLength = ShortestStringLength(strs);
+			for (int i = ZERO; i < minLength; i++)
+			{
+				bool toBreak = false;
+				prefix += strs[ZERO][i];
+				for (int j = ZERO; j < strs.Length; j++)
+				{
+					if (!strs[j][i].Equals(prefix[i])) {
+						prefix = prefix.RemoveLastChar();
+						toBreak = true;
+						break;
+					}
+					else continue;
+				}
+				if (toBreak) break;
+			}
+			return prefix;
+		}
+		public static int ShortestStringLength(string[] strings)
+		{
+			if (strings.Length == ZERO) return ZERO;
+			int length = strings.First().Length;
+			for (int i = ZERO; i < strings.Length; i++)
+			{
+				int currLength = strings[i].Length;
+				if (currLength < length) length = currLength;
+			}
+			return length;
+		}
+	}
+	public static class MyArrayExtension
+	{
+		public static string RemoveLastChar(this string str)
+		{
+			List<char> strArr = str.ToList();
+			strArr.RemoveAt(strArr.Count - 1);
+			return strArr.ToArray().CharArrayToString();
+		}
+
+		public static string CharArrayToString(this char[] array)
+		{
+			string outp = "";
+			for (int i = Solution.ZERO; i < array.Length; i++) outp += array[i];
+			return outp;
+		}
+	}
+}
---
+++ longcompref/longcomprefTests/SolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using longcomprefProj;
+
+namespace longcomprefTests
+{
+    public class SolutionTests
+    {
+        Solution sol = new Solution();
+        [Fact] public void Example1Test() => Assert.Equal("fl", sol.LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+        [Fact] public void Example2Test() => Assert.Equal("", sol.LongestCommonPrefix(new string[] { "dog", "racecar", "car" }));
+        [Fact] public void MyOwnTest1() => Assert.Equal("", sol.LongestCommonPrefix(new string[] { "good", "better", "best" }));
+        [Fact] public void MyOwnTest2() => Assert.Equal("long", sol.LongestCommonPrefix(new string[] { "long", "longer", "longest" }));
+        [Fact] public void MyOwnTest3() => Assert.Equal("alone", sol.LongestCommonPrefix(new string[] { "alone" }));
+    }
+}
